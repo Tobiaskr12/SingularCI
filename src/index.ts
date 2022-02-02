f819aa866e753124659b085ea8776d131183a120
Tobias Kristensen <tobiaskristensen12@gmail.com>
2022-02-02 18:41:01 +0100

    Add the dependency inversion framework InversifyJS

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 import { Logger } from 'tslog';
+import { myContainer } from './inversify-example/inversify.config';
+import { TYPES } from './inversify-example/types';
+import { Warrior } from './inversify-example/interfaces';
 
+const ninja = myContainer.get<Warrior>(TYPES.Warrior);
 const log: Logger = new Logger();
-log.info('No implementation');
+
+log.info(ninja.fight());
+log.info(ninja.sneak());
